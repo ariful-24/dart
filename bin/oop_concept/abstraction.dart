19c680ecf0c6906e35ce53c73ebf81de4aa990b0
ariful-islam <ariful.cse.gub@gmail.com>
2024-11-30 11:57:35 +0600

    creating class and object

--- bin/oop_concept/abstraction.dart
+++ bin/oop_concept/abstraction.dart
@@ -1,3 +1,21 @@
-void main(){
+void main() {
+  // avoiding complexity
+}
 
+class Car {
+  void connectWire() {}
+
+  void connectTyre() {}
+
+  void assembleEngine() {}
+
+  void assemblePlay() {}
+
+  void connectElectricSystem() {}
+
+  void assembleInterior() {}
+
+  void startEngine() {}
+
+  void driveTheCar() {}
 }
