27e37cd5f630697d5f4c521f2f17f84d51a1d670
ariful-islam <ariful.cse.gub@gmail.com>
2024-11-30 18:07:53 +0600

    class and method created

--- bin/oop_concept/abstraction_interface.dart
+++ bin/oop_concept/abstraction_interface.dart
@@ -1,3 +1,24 @@
 void main(){
 
 }
+
+// abstract , extends, implements  --> mechanism(dart)
+
+//Interface
+class Car{
+  void assembleEngine(){}
+
+  void assembleElectricSystem(){}
+
+  void voiceSystem(){}
+}
+
+//Implementation
+
+class Toyota extends Car{
+
+}
+
+class Nissan extends Car{
+
+}
