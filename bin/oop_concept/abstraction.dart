84009e70b8f47b4423f731bd38ceace1694aaa6c
ariful-islam <ariful.cse.gub@gmail.com>
2024-11-30 12:20:55 +0600

    creating object

--- bin/oop_concept/abstraction.dart
+++ bin/oop_concept/abstraction.dart
@@ -1,15 +1,31 @@
 void main() {
-  // avoiding complexity
+  // avoiding complexity / hiding complexity
+
+  Car car = Car('Toyota', 'Allion');
+
+  //Drive your car
+
+  car.assembleDisplay();
+  car.assembleInterior();
+  car.connectWire();
+  car.connectTyre();
+  car.startEngine();
+  car.driveTheCar();
 }
 
 class Car {
+  String brand;
+  String model;
+
+  Car(this.brand, this.model);
+
   void connectWire() {}
 
   void connectTyre() {}
 
   void assembleEngine() {}
 
-  void assemblePlay() {}
+  void assembleDisplay() {}
 
   void connectElectricSystem() {}
 
